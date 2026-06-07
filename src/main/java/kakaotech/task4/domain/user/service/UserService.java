c12baad7f6ca85f0da49b8a7af3ecd785c127ec6
jeongminju <wjdalswn0405@naver.com>
2026-06-08 01:13:44 +0900

    feat: User 레포지토리 관련 비즈니스 로직 수행할 클래스 추가

---
+++ src/main/java/kakaotech/task4/domain/user/service/UserService.java
@@ -0,0 +1,22 @@
+package kakaotech.task4.domain.user.service;
+
+import kakaotech.task4.common.uuid.UuidCreator;
+import kakaotech.task4.common.uuid.UuidPrefix;
+import kakaotech.task4.domain.auth.dto.SignUpRequest;
+import kakaotech.task4.domain.user.entity.User;
+import kakaotech.task4.domain.user.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public void signUp(SignUpRequest request) {
+        String uuid = UuidCreator.create(UuidPrefix.USER);
+        User user = User.of(uuid, request);
+        userRepository.save(user);
+    }
+
+}

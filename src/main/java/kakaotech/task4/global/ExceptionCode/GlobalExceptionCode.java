bebab881f52d6aefba02596c91c5f20c9b3e8b26
jeongminju <wjdalswn0405@naver.com>
2026-06-07 22:19:01 +0900

    feat: 전역 에러 enum 클래스 추가

---
+++ src/main/java/kakaotech/task4/global/ExceptionCode/GlobalExceptionCode.java
@@ -0,0 +1,17 @@
+package kakaotech.task4.global.ExceptionCode;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum GlobalExceptionCode implements ExceptionCode {
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "GLOBAL-500-001", "서버 에러입니다. 서버 팀에 문의해주세요."),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "GLOBAL-400-001", "필수 값이 제외되었습니다."),
+    VALIDATION_ERROR(HttpStatus.UNPROCESSABLE_ENTITY, "GLOBAL-422-001", "필드의 유효성 검사가 올바르지 않습니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}

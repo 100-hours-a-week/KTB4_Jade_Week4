af1bd0af8712d671b3a31ebc297f777dd5664358
jeongminju <wjdalswn0405@naver.com>
2026-06-07 22:19:52 +0900

    feat: 회원가입 dto record 클래스 추가

---
+++ src/main/java/kakaotech/task4/auth/dto/SignUpRequest.java
@@ -0,0 +1,31 @@
+package kakaotech.task4.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record SignUpRequest(
+        @NotBlank(message = "이메일을 입력해주세요.")
+        @Email(message = "올바른 이메일 주소 형식을 입력해주세요.")
+        String email,
+
+        @NotBlank(message = "비밀번호를 입력해주세요.")
+        @Pattern(
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).{8,20}$",
+                message = "비밀번호는 8자 이상, 20자 이하이며, 대문자, 소문자, 숫자, 특수문자를 각각 최소 1개 포함해야 합니다."
+        )
+        String password,
+
+        @NotBlank(message = "비밀번호를 입력해주세요.")
+        String checkPassword,
+
+        @NotBlank(message = "닉네임을 입력해주세요.")
+        @Pattern(
+                regexp = "^\\S{1,10}$",
+                message = "띄어쓰기 없이 10글자 이내로 입력해주세요."
+        )
+        String nickname,
+
+        @NotBlank(message = "프로필 사진을 추가해주세요.")
+        String profileImageUrl
+) {}

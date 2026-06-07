b369ad63b93bedc73983998a6368afddf364225a
jeongminju <wjdalswn0405@naver.com>
2026-06-07 22:18:40 +0900

    feat: 예외 응답 공통 포맷 클래스 추가

---
+++ src/main/java/kakaotech/task4/global/ExceptionRes.java
@@ -0,0 +1,34 @@
+package kakaotech.task4.global;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import kakaotech.task4.global.ExceptionCode.ExceptionCode;
+import lombok.Builder;
+import org.springframework.http.HttpStatus;
+
+import java.util.Map;
+
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ExceptionRes(
+        String code,
+        HttpStatus status,
+        String message,
+        Map<String, Object> fields
+) {
+    public static ExceptionRes from(ExceptionCode error){
+        return ExceptionRes.builder()
+                .status(error.getStatus())
+                .code(error.getCode())
+                .message(error.getMessage())
+                .build();
+    }
+
+    public static ExceptionRes from(ExceptionCode error, Map<String, Object> fields) {
+        return ExceptionRes.builder()
+                .status(error.getStatus())
+                .code(error.getCode())
+                .message(error.getMessage())
+                .fields(fields)
+                .build();
+    }
+}

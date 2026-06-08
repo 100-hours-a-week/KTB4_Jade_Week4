99cce8d1af81e37311833951fff76a78ffc201d0
jeongminju <wjdalswn0405@naver.com>
2026-06-08 22:09:46 +0900

    refactor: 사용하지 않는 enum 제거

--- src/main/java/kakaotech/task4/domain/article/code/ArticleExceptionCode.java
+++ src/main/java/kakaotech/task4/domain/article/code/ArticleExceptionCode.java
@@ -8,7 +8,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ArticleExceptionCode implements ExceptionCode {
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "ARTICLE-401-001", "로그인 후 사용할 수 있습니다.");
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "ARTICLE-401-001", "로그인 후 사용할 수 있습니다."),
+    FORBIDDEN_UPDATE(HttpStatus.FORBIDDEN, "ARTICLE-403-001", "수정 권한이 없습니다."),
+    FORBIDDEN_DELETE(HttpStatus.FORBIDDEN, "ARTICLE-403-002", "삭제 권한이 없습니다."),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE-404-001", "해당 게시글을 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String code;

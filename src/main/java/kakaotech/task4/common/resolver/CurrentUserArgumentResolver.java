f3e66de01813e3c6fd569d7889666e88a800b1a1
jeongminju <wjdalswn0405@naver.com>
2026-06-10 16:31:37 +0900

    feat: ArgumentResolver로 현재 사용자 주입 방식 도입

---
+++ src/main/java/kakaotech/task4/common/resolver/CurrentUserArgumentResolver.java
@@ -0,0 +1,44 @@
+package kakaotech.task4.common.resolver;
+
+import kakaotech.task4.common.exception.CustomException;
+import kakaotech.task4.common.exception.ExceptionCode.GlobalExceptionCode;
+import kakaotech.task4.domain.user.entity.User;
+import kakaotech.task4.domain.user.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.util.Objects;
+
+@Component
+@RequiredArgsConstructor
+public class CurrentUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final UserService userService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(CurrentUser.class)
+                && parameter.getParameterType().equals(User.class);
+    }
+
+    @Override
+    public Object resolveArgument(@NonNull MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) {
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+        String userUuid = Objects.requireNonNull(request).getHeader("Authorization");
+
+        if (userUuid == null || userUuid.isBlank()) {
+            throw new CustomException(GlobalExceptionCode.UNAUTHORIZED);
+        }
+        return userService.findByUuid(userUuid, GlobalExceptionCode.INTERNAL_SERVER_ERROR);
+    }
+}

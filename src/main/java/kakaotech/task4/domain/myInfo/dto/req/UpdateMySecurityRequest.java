9650734abb857dc5a6dd41c94b8b853506d3e367
jeongminju <wjdalswn0405@naver.com>
2026-07-16 11:34:12 +0900

    feat: 비밀번호 변경 시 현재 비밀번호 검증

--- src/main/java/kakaotech/task4/domain/myInfo/dto/req/UpdateMySecurityRequest.java
+++ src/main/java/kakaotech/task4/domain/myInfo/dto/req/UpdateMySecurityRequest.java
@@ -6,19 +6,27 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record UpdateMySecurityRequest(
-        @Schema(description = "비밀번호", example = "JadeHello1234!")
+        @Schema(description = "현재 비밀번호", example = "JadeNow1234!")
         @NotBlank(message = "비밀번호를 입력해주세요.")
         @Pattern(
                 regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).{8,20}$",
                 message = "비밀번호는 8자 이상, 20자 이하이며, 대문자, 소문자, 숫자, 특수문자를 각각 최소 1개 포함해야 합니다."
         )
-        String password,
+        String nowPassword,
 
-        @Schema(description = "비밀번호 확인", example = "JadeHello1234!")
+        @Schema(description = "변경할 비밀번호", example = "JadeNext1234!")
+        @NotBlank(message = "비밀번호를 입력해주세요.")
+        @Pattern(
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).{8,20}$",
+                message = "비밀번호는 8자 이상, 20자 이하이며, 대문자, 소문자, 숫자, 특수문자를 각각 최소 1개 포함해야 합니다."
+        )
+        String nextPassword,
+
+        @Schema(description = "변경할 비밀번호 확인", example = "JadeNext1234!")
         @NotBlank(message = "비밀번호 확인을 입력해주세요.")
-        String checkPassword
+        String checkNextPassword
 ) {
     public boolean validatePasswordMatch() {
-        return password.equals(checkPassword);
+        return nextPassword.equals(checkNextPassword);
     }
 }
